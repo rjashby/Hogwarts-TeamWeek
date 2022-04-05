88dfbcd40f3df384505d3fd6a5716c11a1a60223
Ryan Ashby <rjapsu160@gmail.com>
2022-04-04 18:36:23 -0700

    ensure all views and database update methods are working
    Co-authored-by: Daniel Lindsey <dlinds@users.noreply.github.com>
    Co-authored-by: lionturtle99 <lionturtle99@users.noreply.github.com>
    Co-authored-by: S-Espinet <S-Espinet@users.noreply.github.com>
    Co-authored-by: Anastasiia Riabets <anastasiia-ria@users.noreply.github.com>

--- Hogwarts/Controllers/TeachersController.cs
+++ Hogwarts/Controllers/TeachersController.cs
@@ -40,6 +40,15 @@ namespace Hogwarts.Controllers
         _db.SaveChanges();
         return RedirectToAction("Create");
       }
+
+      public ActionResult Details(int id)
+      {
+          var thisTeacher = _db.Teachers
+            .Include(s => s.JoinEntitiesCT)
+            .ThenInclude(j => j.Course)
+            .FirstOrDefault(s => s.TeacherId == id);
+          return View(thisTeacher);
+      }
       
       public ActionResult Edit(int id)
       {
@@ -52,6 +61,54 @@ namespace Hogwarts.Controllers
       {
         _db.Entry(teacher).State = EntityState.Modified;
         _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+      public ActionResult Delete(int id)
+      {
+        var thisTeacher = _db.Teachers.FirstOrDefault(teacher => teacher.TeacherId == id);
+        return View(thisTeacher);
+      }
+
+      [HttpPost, ActionName("Delete")]
+      public ActionResult DeleteConfirmed(int id)
+      {
+        var thisTeacher = _db.Teachers.FirstOrDefault(teacher => teacher.TeacherId == id);
+        _db.Teachers.Remove(thisTeacher);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+      public ActionResult AddCourse(int id)
+      {
+        var thisTeacher = _db.Teachers.FirstOrDefault(teacher => teacher.TeacherId == id);
+        var thisCourseTeacher = _db.CourseTeacher.Where(courseStudent => courseStudent.TeacherId == id);
+        
+        List<Course> courses = _db.Courses.ToList();
+        List<Course> courses2 = _db.Courses.ToList();
+
+        foreach (CourseTeacher courseTeacher in thisCourseTeacher)
+        {
+          foreach(Course course in courses)
+          {
+            if (course.CourseId == courseTeacher.CourseId)
+            {
+              courses2.Remove(course);
+            }
+          }
+        } 
+        ViewBag.CourseId = new SelectList(courses2, "CourseId", "CourseName");
+        return View(thisTeacher);
+      } 
+
+      [HttpPost]
+      public ActionResult AddCourse(Teacher teacher, int CourseId)
+      {
+        if (CourseId != 0)
+        {
+          _db.CourseTeacher.Add(new CourseTeacher() { TeacherId = teacher.TeacherId, CourseId = CourseId });
+          _db.SaveChanges();
+        }
         return RedirectToAction("Index");
       }
     }
